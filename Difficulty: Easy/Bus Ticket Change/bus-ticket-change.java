ccb56aed405c50f9d345683c1b59573d6884f7d9
Vishal Raut <155873551+VishalRaut2106@users.noreply.github.com>
2026-01-13 23:38:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canServe(int[] arr) {
+        
+        //code here
+        int five = 0, ten = 0;
+
+        for (int bill : arr) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
